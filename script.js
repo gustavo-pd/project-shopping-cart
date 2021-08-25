5d95a46b9dbab1bd494c6ca242290aa9850c56e9
GustavoPD <gudalmoro@gmail.com>
2021-08-25 19:28:32 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -12,23 +12,38 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement(sku, name, image) {
   const section = document.createElement('section');
+  const getSectionList = document.querySelector('.items');
   section.className = 'item';
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
+  getSectionList.appendChild(section);
+  return getSectionList;
 }
+
+const createListML = async (toSearch) => {
+  const productList = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${toSearch}`);
+  const productListJson = await productList.json();
+    productListJson.results.forEach((product) => {
+      createProductItemElement(product.id, product.title, product.thumbnail);
+    });
+};
+
+window.onload = () => {
+createListML('computador');
+};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
+getSkuFromProductItem();
+
+function cartItemClickListener() {
   // coloque seu código aqui
 }
 
@@ -40,4 +55,4 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+createCartItemElement();
